21930549da52d72ada62a579c5cf9abcf074583a
Edward Stoner <edwardlstoner@gmail.com>
2020-04-16 17:30:23 -0600

    Remove fetch from app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,23 +4,9 @@ import Hero from "../Hero/Hero";
 import Header from "../Header/Header";
 
 class App extends Component {
-  componentDidMount() {
-    fetch(
-      `https://www.mtbproject.com/data/get-trails?lat=39.6403&lon=-106.3742&maxDistance=10&key=${process.env.REACT_APP_MTBPROJECT_API_KEY}`
-    )
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-
-    fetch(
-      `http://api.openweathermap.org/data/2.5/weather?id=5442727&appid=${process.env.REACT_APP_OPENWEATHERMAP_API_KEY}`
-    )
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-  }
-
   render() {
     return (
-      <div class="app">
+      <div className="app">
         <Header />
         <Hero />
       </div>
