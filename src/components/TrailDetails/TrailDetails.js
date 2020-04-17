6104f3f0d544c7b5b6030d38fe8973205ec0750f
Edward Stoner <edwardlstoner@gmail.com>
2020-04-17 17:27:04 -0600

    Create Details component

--- src/components/TrailDetails/TrailDetails.js
+++ src/components/TrailDetails/TrailDetails.js
@@ -1,20 +1,44 @@
 import React from "react";
 import "./TrailDetails.css";
 import { connect } from "react-redux";
+import Map from "../Map/Map";
+
+const numberWithCommas = (x) => {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
 
 const TrailDetails = (props) => {
   const selectedTrail = props.trails.find(
     (trail) => trail.id === parseInt(props.id)
   );
-  console.log(selectedTrail);
   return (
-    <div>
-      <div>{selectedTrail.name}</div>
-      <div>{selectedTrail.summary}</div>
-      <div>Length: {selectedTrail.length}</div>
-      <div>Ascent: {selectedTrail.ascent}</div>
-      <div>Descent: {selectedTrail.descent}</div>
-      <img src={selectedTrail.imgMedium} alt={selectedTrail.name} />
+    <div className="trail-detail-card">
+      <div className="trail-stats-container">
+        <div className="details-title">{selectedTrail.name}</div>
+        <div className="details-summary">{selectedTrail.summary}</div>
+        <div className="stats-map-container">
+          <div className="map-container-details">
+            <Map />
+          </div>
+          <div className="secondary-stats-container">
+            <div className="details-length details-item">
+              Length: {selectedTrail.length} miles
+            </div>
+            <div className="details-ascent details-item">
+              Ascent: {numberWithCommas(selectedTrail.ascent)} feet
+            </div>
+            <div className="details-descent details-item">
+              Descent: {numberWithCommas(Math.abs(selectedTrail.descent))} feet
+            </div>
+            <button className="more-info">More info</button>
+          </div>
+        </div>
+      </div>
+      <img
+        className="details-photo"
+        src={selectedTrail.imgMedium}
+        alt={selectedTrail.name}
+      />
     </div>
   );
 };
