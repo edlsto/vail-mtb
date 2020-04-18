b67422dab8d09ded1e2adbbca466e808c3f9d317
Edward Stoner <edwardlstoner@gmail.com>
2020-04-18 14:08:24 -0600

    Add link to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./Header.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <header>vAiL mTb</header>;
+  return (
+    <header>
+      <Link to="/" className="text">
+        vAiL mTb
+      </Link>
+    </header>
+  );
 };
 
 export default Header;
