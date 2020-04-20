f15ffee9d4e323722fd5b107690780e1db42786d
Edward Stoner <edwardlstoner@gmail.com>
2020-04-19 18:07:35 -0600

    Create helper functions

---
+++ src/helpers.js
@@ -0,0 +1,17 @@
+export const widthOfString = (str) => {
+  var canvas = document.createElement("canvas");
+  var ctx = canvas.getContext("2d");
+  ctx.font = "32px Rubik";
+  var width = ctx.measureText(str).width;
+  return width;
+};
+
+export const capitalize = (str) => {
+  let strArray = str.split("");
+  strArray[0] = strArray[0].toUpperCase();
+  return strArray.join("");
+};
+
+export const convertToFahrenheit = (celsius) => {
+  return Math.floor(celsius * (9 / 5) + 32);
+};
