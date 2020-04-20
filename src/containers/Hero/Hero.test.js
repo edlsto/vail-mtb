e7daba4921cbde75208b9c3535f7c3dc46a64904
Edward Stoner <edwardlstoner@gmail.com>
2020-04-20 09:10:35 -0600

    Add default tests

---
+++ src/containers/Hero/Hero.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Hero from "./Hero";
+import { BrowserRouter } from "react-router-dom";
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { rootReducer } from "../../reducers";
+
+describe("trails area tests", () => {
+  it("should", () => {});
+});
