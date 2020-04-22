00d4dd6581c09602557931d12dc6ffdb615b8503
Edward Stoner <edwardlstoner@gmail.com>
2020-04-22 15:48:09 -0600

    Updates

---
+++ precache-manifest.fb50a8636af55c390e4096cb0c96e133.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efbb8645104e96cd3d830ed79fb1168d",
+    "url": "/vail-mtb/index.html"
+  },
+  {
+    "revision": "f88f8c109ccc55fd8985",
+    "url": "/vail-mtb/static/css/main.bb9bb088.chunk.css"
+  },
+  {
+    "revision": "3d0303401a3ba63f0490",
+    "url": "/vail-mtb/static/js/2.32fd4c8f.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/vail-mtb/static/js/2.32fd4c8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f88f8c109ccc55fd8985",
+    "url": "/vail-mtb/static/js/main.1204a0f6.chunk.js"
+  },
+  {
+    "revision": "ae7e067b8ed18164e249",
+    "url": "/vail-mtb/static/js/runtime-main.d9a9c322.js"
+  },
+  {
+    "revision": "9217b751754eedb8e3f39a6dfff0bab7",
+    "url": "/vail-mtb/static/media/aspens.9217b751.jpg"
+  },
+  {
+    "revision": "50f2cfbff0d9333538a7d0ab6eb7e04d",
+    "url": "/vail-mtb/static/media/bike.50f2cfbf.jpg"
+  },
+  {
+    "revision": "4f5a104a0ffc6f894175adb8d659a18e",
+    "url": "/vail-mtb/static/media/down.4f5a104a.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/vail-mtb/precache-manifest.fb50a8636af55c390e4096cb0c96e133.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/vail-mtb/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
